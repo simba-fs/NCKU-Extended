2bf28e33db2e238075cc0af2626b5c714a9a16d1
simba-fs <simba.fs@gmail.com>
2022-06-30 23:48:13 +0800

    add average score and average GPA

--- script/eportfolio.js
+++ script/eportfolio.js
@@ -20,9 +20,16 @@ co_nu.forEach((i, index) => {
  * GPA calculator
  */
 
+// util functions
+
+function getScore(e){
+	let score = parseInt(e.children[7].innerText);
+	return [!!score, score || 0];
+}
+
 function getGpa(e){
-	let score = parseInt(e.lastElementChild.innerText);
-	if(isNaN(score)) return '成績未到';
+	let score = getScore(e)[1];
+	if(isNaN(score)) return 0;
 	let boundary = [
 		[90, 4.3],
 		[85, 4.0],
@@ -40,25 +47,63 @@ function getGpa(e){
 	}
 }
 
-function getCredit(e){
-	return parseInt(e.lastElementChild.previousElementSibling.innerText);
+function getCreditHour(e){
+	return parseInt(e.children[6].innerText) || 0;
 }
 
-console.log('loaded');
-[...document.querySelectorAll('tr')].slice(0, -1).forEach((i, index) => {
-	let gpa = getGpa(i);
-	let credit = getCredit(i);
+function to2(n){
+	return Math.round(n * 100) / 100;
+}
 
-	let gpaElement = document.createElement(index === 0 ? 'th' : 'td');
-	gpaElement.innerText = index === 0 ?
-		'GPA' : gpa;
+let totalScore = 0;
+let totalGpa = 0;
+let totalCreditHour = 0;
+
+// head
+document.querySelectorAll('thead > tr').forEach(e => {
+	let gpa = document.createElement('th');
+	gpa.innerText = 'GPA';
+	let creditHour = document.createElement('th');
+	creditHour.innerText = 'GPA*學分';
+	e.append(gpa);
+	e.append(creditHour);
+});
+
+// body
+[...document.querySelectorAll('tbody > tr')].slice(0, -1).forEach((i, index) => {
+	let [hasScore, score] = getScore(i);
+	let gpa = getGpa(i);
+	let creditHour = getCreditHour(i);
+	// console.log({i, hasScore, score, gpa, creditHour});
+
+	if(hasScore){
+		totalScore += score * creditHour;
+		totalGpa += gpa*creditHour;
+		totalCreditHour += creditHour;
+	}
+
+	let gpaElement = document.createElement('td');
+	gpaElement.innerText = hasScore ? gpa : '成績未到';
 	i.append(gpaElement);
 
-	let creditElement = document.createElement(index === 0 ? 'th' : 'td');
-	credit = (gpa*credit).toPrecision(2);
-	creditElement.innerText = index === 0 ? 
-		'GPA*學分' : credit === 'NaN' ?
-			'成績未到' : credit;
+	let creditElement = document.createElement('td');
+	let credit = to2(gpa*creditHour);
+	creditElement.innerText = hasScore ? credit : '成績未到';
 	i.append(creditElement);
 });
-document.querySelector('tbody > tr:last-child > td').colSpan = 10;
+
+// average
+// console.log({totalScore, totalGpa, totalCreditHour});
+document.querySelector('tbody > tr:last-child > td').colSpan = 7;
+document.querySelectorAll('tbody>tr:last-child').forEach(e => {
+	let averageScore = document.createElement('td');
+	averageScore.innerText = to2(totalScore/totalCreditHour);
+	e.append(averageScore);
+
+	let averageGpa = document.createElement('td');
+	averageGpa.innerText = to2(totalGpa/totalCreditHour);
+	averageGpa.colSpan = 2;
+	averageGpa.align = 'center';
+	e.append(averageGpa);
+});
+
